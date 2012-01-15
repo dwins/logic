0bb2d96756863f28b257ffe083bd9313c1acd0d2
David Winslow <cdwinslow@gmail.com>
2012-01-15 16:00:46 -0500

    Add stricter properties to Knowledge spec

--- src/test/scala/logic/KnowledgeSpecification.scala
+++ src/test/scala/logic/KnowledgeSpecification.scala
@@ -1,14 +1,14 @@
 package logic
 
 import org.scalacheck._
-import Arbitrary.arbitrary, Prop.forAll
+import Arbitrary.arbitrary, Prop._
 
 object KnowledgeSpecification extends Properties("Knowledge") {
   import Generators._
   import Knowledge.Oblivion._
   import Sentence._
 
-  def atoms(s: Sentence): Set[Atom] = {
+  def atomsIn(s: Sentence): Set[Atom] = {
     def helper(s: Sentence, accum: Set[Atom]): Set[Atom] = 
       s match {
         case True | False => accum
@@ -19,6 +19,35 @@ object KnowledgeSpecification extends Properties("Knowledge") {
       }
 
     helper(s, Set.empty)
+  }
+
+  type Assignment = Map[Atom, Boolean]
+
+  def truthTable(s: Sentence)(atoms: Set[Atom] = atomsIn(s))
+  : Map[Assignment, Boolean]
+  = {
+    require(atomsIn(s) subsetOf atoms)
+
+    val allFalse = (atoms map (_ -> false) toMap)
+    val assignments = 
+      for (subset <- atoms.subsets) yield
+        allFalse ++ (subset map (_ -> true))
+
+    def evaluate(as: Assignment): Boolean = {
+      def eval(s: Sentence): Boolean =
+        s match {
+          case False => false
+          case True => true
+          case p @ Atom(_) => as(p)
+          case ¬(s) => !(eval(s))
+          case And(p, q) => eval(p) && eval(q)
+          case Or(p, q) => eval(p) || eval(q)
+        }
+
+      eval(s)
+    }
+
+    assignments.map(a => (a, evaluate(a))).toMap
   }
 
   def size(s: Sentence): Int = {
@@ -35,14 +64,38 @@ object KnowledgeSpecification extends Properties("Knowledge") {
 
   property("Reduction never increases sentence size") = 
     forAll { (s: Sentence) => size(reduce(s)) <= size(s) }
+
   property("Reduction never introduces terms") =
-    forAll { (s: Sentence) => atoms(reduce(s)) subsetOf atoms(s) }
+    forAll { (s: Sentence) => atomsIn(reduce(s)) subsetOf atomsIn(s) }
+
+  property("Reduction never alters the truth table") =
+    forAll { (s: Sentence) => 
+      val atoms = atomsIn(s)
+      truthTable(s)(atoms) == truthTable(reduce(s))(atoms)
+    }
+
   property("Conjunction with negation") =
     forAll { (s: Sentence) => reduce(s ∧ ¬(s)) == False }
+
   property("Disjunction with negation") =
     forAll { (s: Sentence) => reduce(s ∨ ¬(s)) == True }
+
   property("Conjunction with self") =
-    forAll { (s: Sentence) => reduce(s ∧ s) == reduce(s) }
+    forAll { (s: Sentence) =>
+      truthTable(reduce(s ∧ s))(atomsIn(s)) == truthTable(s)(atomsIn(s))
+    }
+
   property("Disjunction with self") =
-    forAll { (s: Sentence) => reduce(s ∨ s) == reduce(s) }
-}
+    forAll { (s: Sentence) => 
+      truthTable(reduce(s ∨ s))(atomsIn(s)) == truthTable(s)(atomsIn(s))
+    }
+
+  // TODO: Rewrite this to construct implied sentences instead of hoping to
+  //       receive them randomly
+  // property("Implication is transitive") =
+    forAll { (p: Sentence, q: Sentence, r: Sentence) => 
+      ((reduce(p) != False && reduce(q) != False) &&
+      given(p).reduce(q) == True &&
+      given(q).reduce(r) == True) ==> (given(p).reduce(r) == True)
+    }
+} 
