b519927cd211748f39349f5abb2f9a6417302920
David Winslow <cdwinslow@gmail.com>
2012-01-17 22:52:30 -0500

    Beef up the shrinking rules for scalacheck

--- src/test/scala/logic/Generators.scala
+++ src/test/scala/logic/Generators.scala
@@ -1,7 +1,7 @@
 package logic
 
 import Symbolic.{ Sentence, Atom }, Sentential.symbolicSentences._, Ops._
-import org.scalacheck._, Arbitrary.arbitrary
+import org.scalacheck._, Arbitrary.arbitrary, Shrink.shrink
 
 object Generators {
   implicit lazy val arbitrarySentence: Arbitrary[Sentence] = {
@@ -38,8 +38,14 @@ object Generators {
 
   implicit lazy val shrinkSentence: Shrink[Sentence] = 
     Shrink {
-      case And(p, q) => Stream(p, q)
+      case And(p, q) =>
+        Stream(p, q) append
+        (for { p2 <- shrink(p) ; p3 <- Seq(p2, And(p2, q)) } yield p3) append
+        (for { q2 <- shrink(q) ; q3 <- Seq(q2, And(p, q2)) } yield q3)
       case Or(p, q) => Stream(p, q)
+        Stream(p, q) append
+        (for { p2 <- shrink(p) ; p3 <- Seq(p2, Or(p2, q)) } yield p3) append
+        (for { q2 <- shrink(q) ; q3 <- Seq(q2, Or(p, q2)) } yield q3)
       case ¬(p) => Stream(p)
       case Atom(_) => Stream(False, True)
       case _ => Stream.empty
