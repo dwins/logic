668ad3f48acd008f0d958471fee94d9ef20c011d
David Winslow <cdwinslow@gmail.com>
2012-01-18 20:59:46 -0500

    Split up implementation across a few files

---
+++ src/main/scala/logic/Symbolic.scala
@@ -0,0 +1,25 @@
+package logic
+
+object Symbolic {
+  sealed trait Sentence
+  case object False extends Sentence
+  case object True extends Sentence
+
+  case class Atom(s: Symbol) extends Sentence {
+    override def toString = s.toString
+  }
+
+  case class Not(p: Sentence) extends Sentence {
+    override def toString = "¬" + p.toString
+  }
+
+  case class And(p: Sentence, q: Sentence) extends Sentence {
+    override def toString = "(%s ∧ %s)" format(p, q)
+  }
+
+  case class Or(p: Sentence, q: Sentence) extends Sentence {
+    override def toString = "(%s ∨ %s)" format(p, q)
+  }
+
+  implicit def symbolAsAtom(s: Symbol): Sentence = Atom(s)
+}
